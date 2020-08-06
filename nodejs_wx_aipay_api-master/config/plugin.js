3c4439140aa99cad9844c98a3d8825a3e6b6d254
dufulibai <31023609555@qq.com>
2020-08-06 22:20:54 +0800

    wxpay

---
+++ nodejs_wx_aipay_api-master/config/plugin.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// had enabled by egg
+// exports.static = true;
+
+exports.sequelize = {
+  enable: true,
+  package: 'egg-sequelize',
+};
+
+exports.validate = {
+  enable: true,
+  package: 'egg-validate',
+};
---
+++ nodejs_wx_aipay_api-master/database/migrations/20190118093805-init-yio_orders.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const { INTEGER, DATE, DECIMAL, STRING, ENUM } = Sequelize;
+    await queryInterface.createTable('yio_orders', {
+      id: { type: INTEGER(11), primaryKey: true, autoIncrement: true },
+      order_id: { type: STRING(50), allowNull: false },
+      order_type: { type: ENUM('wechat', 'alipay'), defaultValue: 'wechat', allowNull: false },
+      order_price: { type: DECIMAL(7, 2), allowNull: false },
+      order_name: { type: STRING(255), allowNull: false },
+      pay_status: { type: ENUM('未支付', '已支付', '已过期'), defaultValue: '未支付', allowNull: false },
+      qr_url: { type: STRING(255), allowNull: false },
+      qr_price: { type: DECIMAL(7, 2), allowNull: false },
+      redirect_url: { type: STRING(255), allowNull: false },
+      extension: { type: STRING(255) },
+      created_at: { type: DATE, allowNull: false },
+      updated_at: { type: DATE, allowNull: false },
+      deleted_at: { type: DATE },
+    });
+  },
+
+  down: async queryInterface => {
+    await queryInterface.dropTable('yio_orders');
+  },
+};
